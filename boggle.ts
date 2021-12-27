ba5591c436c7bff16f3c61705585d7f1850b5998
Gergely Kota <gergelyk@gmail.com>
2021-12-26 23:13:35 -0800

    Runner and refactor

--- boggle.ts
+++ boggle.ts
@@ -1,36 +1,15 @@
 //import { Board, Deck, Dice, Algo1 } from "./index";
-import { Deck, Dice, Board } from "./index";
+import { Algo1 } from "./index";
+import { getBoard } from "./BoggleBoard";
+import wordListPath from "word-list";
+console.log(`path: ${wordListPath}`)
+const fs = require("fs");
+const wordArray: string[] = fs.readFileSync(wordListPath, 'utf8').split('\n');
+//console.log(`${wordArray.length} words: ${wordArray.slice(0, 20)}`)
 
-
-const allTheDice = [
-    new Dice("soiste".split("")),
-    new Dice("ngeeaa".split("")),
-    new Dice("ueesni".split("")),
-    new Dice("hrvtew".split("")),
-    new Dice("wtoota".split("")),
-    new Dice("cmuoit".split("")),
-    new Dice("eehnwg".split("")),
-    new Dice("obbajo".split("")),
-    new Dice("zhrlnn".split("")),
-    new Dice("eltytr".split("")),
-    new Dice("pshaco".split("")),
-    new Dice("dryvle".split("")),
-    new Dice("rliexd".split("")),
-    new Dice("ytdtsi".split("")),
-    new Dice(["m","u","h","n","i","qu"]),
-    new Dice("aspfkf".split("")),
-];
-
-// so this is the set of dice
-const deck = new Deck(allTheDice).shuffle();
-const roll = deck.elements.map(dice => dice.roll());
-const sq: string[][] = [];
-for (let i = 0; i < 4; i++) {
-    const r = [];
-    for (let j = 0; j < 4; j++) {
-        r.push(roll[i*4+j]);
-    }
-    sq.push(r);
-}
-const board = new Board(sq);
-console.log(board)
+const board = getBoard();
+console.log(board);
+const algo = new Algo1(wordArray);
+const found = algo.findWords(board);
+const sortedFound = found.sort((a,b) => b.length - a.length);
+console.log(sortedFound);
