1639b49b44d5732e67794178e72e9fe8b8c609b9
Gergely Kota <gergelyk@gmail.com>
2021-12-27 15:09:13 -0800

    Add boggleDefine

--- BoggleWords.ts
+++ BoggleWords.ts
@@ -4,3 +4,8 @@ export function getLegalWords() {
     const wordArray: string[] = Object.keys(dictionary);
     return wordArray;
 }
+
+export function getWordDefinition(word: string) {
+    const def = dictionary[word];
+    return def;
+}
