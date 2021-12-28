514d37640aaaa5942aa2a839a9a5c63e2fcbdfd7
Gergely Kota <gergelyk@gmail.com>
2021-12-27 16:46:09 -0800

    Obs is for definition, not word

--- BoggleWords.ts
+++ BoggleWords.ts
@@ -2,8 +2,7 @@ import { dictionary } from "./dictionary";
 
 export function getLegalWords() {
     const wordArray: string[] = Object.keys(dictionary);
-    const filtered = wordArray.filter(word => dictionary[word].indexOf("[Obs.]") < 0);
-    return filtered;
+    return wordArray;
 }
 
 export function getWordDefinition(word: string) {
